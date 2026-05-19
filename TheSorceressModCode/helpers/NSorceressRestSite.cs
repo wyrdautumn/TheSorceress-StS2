5e414ca4767b9f49f24969775c9cf55a7fed850a
Autumn Jones <notpigeonv2@gmail.com>
2026-05-19 00:07:07 -0400

    Added merchant and camp site visuals. Soon Placeholder Silent will be gone. I made the combat sprite smaller--she's actually supposed to be kinda short. Fixed a few localization errors.

---
+++ TheSorceressModCode/helpers/NSorceressRestSite.cs
@@ -0,0 +1,10 @@
+using Godot;
+using MegaCrit.Sts2.Core.Nodes.RestSite;
+
+namespace TheSorceressMod.TheSorceressModCode.helpers;
+
+[GlobalClass]
+public partial class NSorceressRestSite : NRestSiteCharacter
+{
+	
+}
---
+++ TheSorceressModCode/helpers/NSorceressSelectionReticle.cs
@@ -0,0 +1,10 @@
+using Godot;
+using MegaCrit.Sts2.Core.Nodes.Combat;
+
+namespace TheSorceressMod.TheSorceressModCode.helpers;
+
+[GlobalClass]
+public partial class NSorceressSelectionReticle : NSelectionReticle
+{
+	
+}
