f2a9ae2fcf2b9f3ece1d567a620034cb0122950f
Autumn Jones <notpigeonv2@gmail.com>
2026-06-17 08:35:55 -0400

    Added an image for Primal Flame, shortened the character selection screen description, updated BaseLib, a fun little extra thing.

---
+++ TheSorceressModCode/Patches/SovereignBladePortraitPatch.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Cards;
+
+namespace TheSorceressMod.TheSorceressModCode.Patches;
+
+[HarmonyPatch]
+public class SovereignBladePortraitPatch
+{
+    [HarmonyPatch(typeof(CardModel), nameof(CardModel.PortraitPath), MethodType.Getter)]
+    [HarmonyPostfix]
+    public static void SovereignBladeSorceressPortraitPostfix(CardModel __instance, ref string __result)
+    {
+        if (__instance is SovereignBlade && __instance.IsInCombat && __instance.Owner.Character is Character.TheSorceressMod)
+        {
+            __result = "res://TheSorceressMod/images/card_portraits/sovereign_blade_bird.png";
+        }
+    }
+}
